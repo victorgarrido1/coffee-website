38cbda02b600fd6b058a2004c0f7311c8e0562d6
victorgarrido1 <vcgarr94@gmail.com>
2024-02-01 19:33:35 -0700

    setting up file structure

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 //dependencies
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
-const sequelize = require('
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+//HandelBars engine to use the the helper functions
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars'. hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
