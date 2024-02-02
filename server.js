b51a72f857020cfe71f4b1eb56decfd907049ae7
victorgarrido1 <vcgarr94@gmail.com>
2024-02-01 19:11:40 -0700

    file strcuture

---
+++ server.js
@@ -0,0 +1,8 @@
+//dependencies
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('
